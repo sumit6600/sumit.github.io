47f7dcecfc29e9fb6475ddbdf521c90113f6a858
amit singh <singhamitsingh343@gmail.com>
2022-11-11 12:56:41 +0530

    hh

--- pages.js
+++ pages.js
@@ -1,6 +1,6 @@
 const ghpages = require("gh-pages");
 const pathname = `${__dirname}/build`;
-const repoURL = "https://sumit-12.herokuapp.com/";
+const repoURL = "https://github.com/sumit6600/sumit.github.io";
 
 ghpages.publish(
   pathname,
